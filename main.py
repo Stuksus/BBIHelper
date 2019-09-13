f62135cbaf4c92199ad3ed006e46c02c922afbcd
Stuksus <itsoluution@gmail.com>
2019-09-13 16:19:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,79 +25,79 @@ def getNameById(session,userIds):
     teg+=data['first_name']+" "+data['last_name']
     return teg
 
-
-try:
-    vkSession = vk_api.VkApi('+'+"79160873425","123123qq",scope="wall")
-    vkSession.auth()
-
-
-    vk=Auth()
-    LongPoll=getLongPoll(vk)
-
-    def sendNullMembers(sessionP,sessionG,ownerId,pollId,peerId,groupId):
-        membersOfGroup=getMembersOfGroup(sessionG,peerId,groupId)
-        idsGroup=[]
-        for id in membersOfGroup["items"]:
-            if id["member_id"]>0:
-                idsGroup.append(id["member_id"])
-        print(idsGroup)
-        nullVote=[]
-        try:
-            ids = []
-            for id in getMembersOfPoll(sessionP, ownerId, pollId)["friends"]:
-                ids.append(id["id"])
-            print(ids)
-        except:
-            nullVote=idsGroup
-
-        if nullVote!=[]:
-            for i in idsGroup:
-                for j in ids:
-                    if i!=j:
-
-                        nullVote.append(i)
+while True:
+    try:
+        vkSession = vk_api.VkApi('+'+"79160873425","123123qq",scope="wall")
+        vkSession.auth()
 
 
-        print("nullVote")
-        print(nullVote)
-        message=''
-        for i in nullVote:
-            name=getNameById(vk,i)
-            print(name)
-            message+="[id"+str(i)+"|"+str(name)+"]"+", "
-        print(message)
-        res=write_msg(sessionG,peerId,message)
-        print(res)
-    while True:
-        response =requests.get(LongPoll["server"]+"?act=a_check&key="+LongPoll['key']+"&ts="+LongPoll['ts']+"&wait=25").json()
-        updates = response['updates']
-        if updates:
-            for element in updates:
-                print(element)
-                if element['object']['text'].find("club186392580")>=0:
-                    if element['object']['fwd_messages']!=[]:
-                        if element['object']['fwd_messages'][0]['attachments'][0]['type']=='poll':
-                            groupId=element['group_id']
-                            peerId=element['object']['peer_id']
-                            pollInfo=element['object']['fwd_messages'][0]['attachments'][0]['poll']
-                            ownerId=pollInfo['owner_id']
-                            pollId=pollInfo['id']
-                    else:
-                        if element['object']['attachments']!=[]:
-                            if element['object']['attachments'][0]['type']=='poll':
+        vk=Auth()
+        LongPoll=getLongPoll(vk)
+
+        def sendNullMembers(sessionP,sessionG,ownerId,pollId,peerId,groupId):
+            membersOfGroup=getMembersOfGroup(sessionG,peerId,groupId)
+            idsGroup=[]
+            for id in membersOfGroup["items"]:
+                if id["member_id"]>0:
+                    idsGroup.append(id["member_id"])
+            print(idsGroup)
+            nullVote=[]
+            try:
+                ids = []
+                for id in getMembersOfPoll(sessionP, ownerId, pollId)["friends"]:
+                    ids.append(id["id"])
+                print(ids)
+            except:
+                nullVote=idsGroup
+
+            if nullVote==[]:
+                for i in idsGroup:
+                    for j in ids:
+                        if i!=j:
+
+                            nullVote.append(i)
+
+
+            print("nullVote")
+            print(nullVote)
+            message=''
+            for i in nullVote:
+                name=getNameById(vk,i)
+                print(name)
+                message+="[id"+str(i)+"|"+str(name)+"]"+", "
+            print(message)
+            res=write_msg(sessionG,peerId,message)
+            print(res)
+        while True:
+            response =requests.get(LongPoll["server"]+"?act=a_check&key="+LongPoll['key']+"&ts="+LongPoll['ts']+"&wait=25").json()
+            updates = response['updates']
+            if updates:
+                for element in updates:
+                    print(element)
+                    if element['object']['text'].find("club186392580")>=0:
+                        if element['object']['fwd_messages']!=[]:
+                            if element['object']['fwd_messages'][0]['attachments'][0]['type']=='poll':
                                 groupId=element['group_id']
                                 peerId=element['object']['peer_id']
-                                print(groupId)
-                                pollInfo=element['object']['attachments'][0]['poll']
+                                pollInfo=element['object']['fwd_messages'][0]['attachments'][0]['poll']
                                 ownerId=pollInfo['owner_id']
                                 pollId=pollInfo['id']
-                    sendNullMembers(vkSession,vk,ownerId,pollId,peerId,groupId)
-        LongPoll['ts'] = response['ts']
+                        else:
+                            if element['object']['attachments']!=[]:
+                                if element['object']['attachments'][0]['type']=='poll':
+                                    groupId=element['group_id']
+                                    peerId=element['object']['peer_id']
+                                    print(groupId)
+                                    pollInfo=element['object']['attachments'][0]['poll']
+                                    ownerId=pollInfo['owner_id']
+                                    pollId=pollInfo['id']
+                        sendNullMembers(vkSession,vk,ownerId,pollId,peerId,groupId)
+            LongPoll['ts'] = response['ts']
 
 
-except:
-    vk2 = Auth()
-    var = traceback.format_exc()
-    write_msg(vk2,163298402,var)
+    except:
+        vk2 = Auth()
+        var = traceback.format_exc()
+        write_msg(vk2,163298402,var)
 
 
